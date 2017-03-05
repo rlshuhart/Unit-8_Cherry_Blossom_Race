fd55cb5a0327b7bc6bd9e5fb58e66e9ef1707a73
Ryan Shuhart <shuhart@gmail.com>
2017-03-05 17:31:55 -0600

    finalized 1_clean_the_blossoms

--- src/1_clean_the_blossoms.R
+++ src/1_clean_the_blossoms.R
@@ -1,55 +1,46 @@
 library(dplyr)
-library(tidyr)
 library(stringr)
 
-run_data <- readRDS("../data/external/run_data1990-2016.rds")
+external_data <- "run_data1990-2016.rds"
+run_data <- readRDS(paste("../data/external/",external_data, sep=""))
 
-## Age Conversion - NR stands for Not Recorded, replaced with NA
-run_data$AgeInt <- as.numeric(run_data$Age)
-
-## Time Clean Up
-# Some times have * after time - not sure what it means yet, but creating a column to market it
+############################ Time Clean Up ############################ 
+# Some years have asterisk in time - not sure what it means yet, 
+# but creating a column to market it
 run_data$hasTimeAsterisk <- 0
-run_data[grep("\\*", run_data$Time),]$hasTimeAsterisk <- 1
 
-# Remove Asterisk
+# Some 
+if (sum(grep("\\*", run_data$Time)) > 0){
+  run_data[grep("\\*", run_data$Time),]$hasTimeAsterisk <- 1 # Adds indicators of having asterisk
+}
+
+# Remove Asterisk from time
 run_data$Time<-gsub("\\*", "",  run_data$Time)
 
-# add time in seconds
+########################### Time Conversion ###########################
+# add time in minutes
 # problem is the format is inconsistent, some are in mm:ss and some in hh:mm:ss
 # 1. mm:ss only
 # 2. hh:mm:ss only
 # 3. mix of mm:ss and hh:mm:ss
 
-m <- str_split(run_data$Time, ":", simplify = TRUE)
-
-if (ncol(m) == 3){
-  run_data$TimeInSeconds <- if_else(m[,3] == "", as.numeric(m[,1]) * 60 + as.numeric(m[,2]), 
-          as.numeric(m[,1]) * 3600 + as.numeric(m[,2]) * 60 + as.numeric(m[,3]))
-} else if (ncol(m) == 2){
-  run_data$TimeInSeconds <- as.numeric(m[,1]) * 60 + as.numeric(m[,2])
+convert_time <- function(time_vector){
+  m <- str_split(time_vector, ":", simplify = TRUE)
+  
+  if (ncol(m) == 3){ # If problem #2 or #3
+    time_vector <- if_else(m[,3] == "", as.numeric(m[,1]) + as.numeric(m[,2]) / 60, 
+                           as.numeric(m[,1]) * 60 + as.numeric(m[,2]) + as.numeric(m[,3]) / 60)
+  } else if (ncol(m) == 2){ # If problem #1
+    time_vector <- as.numeric(m[,1]) + as.numeric(m[,2]) / 60
+  }
+  
+  return(time_vector)
 }
 
+# Convert time and age from character
+run_data <- run_data %>% mutate(TimeInMin = convert_time(Time),
+                                PaceInMin = convert_time(Pace),
+                                Age = as.numeric(Age))
 
-
-#### Scratch code below ####
-run_data$TimeInSeconds <- as.numeric(m[,1]) * 3600 + as.numeric(m[,2]) * 60 + as.numeric(m[,3])
-run_data$TimeAsDuration <- duration(hours=as.numeric(m[,1]), 
-                                    minutes=as.numeric(m[,2]), 
-                                    seconds= as.numeric(m[,3]))
-
-str(run_data)
-
-run_data$Time[150]
-duration(strptime(run_data$Time[1], "%M:%S"))
-
-run_data$StripTime <- strptime(run_data$Time[118:125], format=guess_formats(run_data$Time[118:125], c("%H:%M:%S","%M:%S")))
-duration(seconds=run_data$TimeInSeconds[1])
-
-run_data %>% 
-  transmute(StripTime = strptime(Time, 
-                                 format=guess_formats(Time, c("%H:%M:%S","%M:%S")))) %>%
-  mutate(TimeAsDuration = as.Date.POSIXct(duration(hours = hour(StripTime),
-                                   minutes = minute(StripTime),
-                                   seconds = second(StripTime)))
-         )
+# Write to processed data
+saveRDS(run_data, paste("../data/processed/processed_", external_data, sep=""))
